01db7fdbcc323dff59017bd963d5c98f0fdda24e
Zamir <zamir.ivry@gmail.com>
2016-07-04 21:35:22 +0300

    Updated at hyperdev.com

--- public/client.js
+++ public/client.js
@@ -6,29 +6,57 @@
 // by default, you've got jQuery,
 // add other scripts at the bottom of index.html
 
+// $("form").submit(function(){
+//     // forks = request(https://fierce-bat.hyperdev.space/forks/angular/angular);
+//   $('#myTable tr:last').after("<tr>username</tr>\
+//     <tr>url</tr> \
+//     <tr>watchers</tr>\
+//     <tr>stars</tr>\
+//     <tr>forks_count</tr>\
+//     <tr>forks</tr>\
+//     <tr>updatedAt</tr>"); 
+//     // forks.data.forEach(function(fork)
+//     // {
+//     //   var row = '';
+//     //   fork.forEach(function(col)
+//     //   {
+//     //     row += '<tr>' + col +'</tr>';                          
+//     //   });
+//     //   $('#myTable tr:last').after(row);
+//     // });
+// });
+var getForks = function() 
+{
+   
+};
 $(function() {
   console.log('hello world :o');
-  $.get('/forks/:owner/:repo'), function(forks)
-  {
-    forks.data.forEach(function(fork)
-    {
-      var row = '';
-      fork.forEach(function(col)
-      {
-        row += '<tr>' + col +'</tr>';                          
-      });
-      $('#myTable tr:last').after(row);
-    });
-  }
-  $.get('/dreams', function(dreams) {
-    
-    dreams.forEach(function(dream) {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-    });
-  });
+ 
 
   $('form').submit(function(event) {
-    event.preventDefault();
+    var rep = $('#reporistory').val();
+    console.log(rep);
+    var u = "https://fierce-bat.hyperdev.space//forks/" + rep;
+     
+     
+    $.ajax({
+        url: u
+    }).then(function(obj) {
+      console.log("called url: " + u);
+     
+      for (var i = 0; i < obj.data.length; i++) {
+        // console.dir(obj.data[i]);   
+        var fork = obj.data[i];
+        $('#myTable tr:last').after("<tr> \
+        <td><a href=" + fork.url + ">" + fork.login + "<a></td>\
+        <td>" + fork.watchers + "</td>\
+        <td>" + fork.stars + "</td>\
+        <td>" + fork.forks_count + "</td>\
+        <td>" + fork.forks + "</td>\
+        <td>" + fork.updated_at + "</td><tr>"); 
+        }
+    });
+   event.preventDefault();
    
    
   });
